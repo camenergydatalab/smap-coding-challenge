943ac1e178a3cf7508deb20e7c9a0fea7745b0d3
shirakia <atsuo.shiraki@gmail.com>
2017-09-10 17:26:21 +0900

    Fix indents

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -7,12 +7,13 @@ from django.shortcuts import render
 
 
 def summary(request):
-  context = {
-      'message': 'Hello!',
-  }
-  return render(request, 'consumption/summary.html', context)
+    context = {
+        'message': 'Hello!',
+    }
+    return render(request, 'consumption/summary.html', context)
+
 
 def detail(request):
-  context = {
-  }
-  return render(request, 'consumption/detail.html', context)
+    context = {
+    }
+    return render(request, 'consumption/detail.html', context)
