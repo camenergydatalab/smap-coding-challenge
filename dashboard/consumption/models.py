d47b1cc0be148da4898d3447350a2e05eccf3a21
yuyasat <yuya.taki@enechange.co.jp>
2018-06-03 14:40:20 +0900

    add consumption migrations

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    area = models.CharField(max_length=4, default='a1')
+    tariff = models.CharField(max_length=4, default='t1')
+
+class Consumption(models.Model):
+    user = models.ForeignKey(User)
+    datetime = models.DateTimeField(default=datetime.now)
+    consumption = models.DecimalField(max_digits=6, decimal_places=2, default=None)
 # Create your models here.
