bda2456f7562f75d42d85480f11bb8826a434593
yuyasat <yuya.taki@enechange.co.jp>
2018-06-03 14:40:42 +0900

    set postgres settings

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['PGDB_NAME'],
+        'USER': os.environ['PGUSER'],
+        'PASSWORD': os.environ['PGPASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
