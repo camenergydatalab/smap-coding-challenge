881d9be625be00ad6532e3708ad075a172159c7c
Masuda Taku <masudat774@gmail.com>
2019-09-16 21:06:02 +0900

    Add import scripts
    When you execute `python manage.py import` in the terminal,
    this script import csv data files in /data/, update databases and
    output all.csv which includes the total consumption (aggregation of all users).

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -1,8 +1,42 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+import warnings
 from django.core.management.base import BaseCommand
+
+from consumption.models import UserReport, ConsumptionData
+
+warnings.filterwarnings("ignore")
 
 
 class Command(BaseCommand):
-    help = 'import data'
+    help = "import data"
 
     def handle(self, *args, **options):
-        print("Implement me!")
+        data_dir = os.path.join(
+            os.path.dirname(__file__), "..", "..", "..", "..", "data"
+        )
+        fname = os.path.join(data_dir, "user_data.csv")
+        user_data_df = pd.read_csv(fname)
+
+        users_list = list(UserReport.objects.all().values_list("user_id", flat=True))
+        total_consumption = ConsumptionData()
+        for idx, user_data in tqdm(user_data_df.iterrows()):
+            user_id = str(user_data["id"])
+            if user_id in users_list:
+                user_report = UserReport.objects.get(user_id=user_id)
+            else:
+                user_report = UserReport()
+            user_report.user_id = user_id
+            user_report.area = user_data["area"]
+            user_report.tariff = user_data["tariff"]
+
+            csv_name = os.path.join(data_dir, "consumption", f"{user_id}.csv")
+            consumption_data = ConsumptionData()
+            consumption_data.import_csv(csv_name)
+            total_consumption.import_csv(csv_name)
+            user_report.consumption_total = consumption_data.total
+            user_report.consumption_avg = consumption_data.avg
+
+            user_report.save()
+        total_consumption.to_csv(os.path.join(data_dir, "consumption", "all.csv"))
--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -1,6 +1,38 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import pandas as pd
 from django.db import models
 
 # Create your models here.
+
+
+class UserReport(models.Model):
+    user_id = models.CharField(max_length=200)
+    area = models.CharField(max_length=8)
+    tariff = models.CharField(max_length=8)
+    consumption_total = models.FloatField(default=0)
+    consumption_avg = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.user_id
+
+
+class ConsumptionData:
+    def __init__(self):
+        self._df = pd.DataFrame(columns=["consumption"])
+
+    def import_csv(self, csv_name):
+        df = pd.read_csv(csv_name).drop_duplicates(subset="datetime")
+        df.set_index("datetime", inplace=True)
+        self._df = self._df.add(df, fill_value=0)
+
+    def to_csv(self, csv_name):
+        self._df.to_csv(csv_name)
+
+    @property
+    def total(self):
+        return self._df["consumption"].sum()
+
+    @property
+    def avg(self):
+        return self._df["consumption"].mean() * 2  # 0.5h/point
