e0559ea0725e597b2cbc8c05ac148ffa060e12c5
Masuda Taku <masudat774@gmail.com>
2019-09-16 22:13:56 +0900

    Add summary page

--- dashboard/consumption/urls.py
+++ dashboard/consumption/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.summary),
-    url(r'^summary/', views.summary),
-    url(r'^detail/', views.detail),
+    url(r"^$", views.SummaryView.as_view()),
+    url(r"^summary/", views.SummaryView.as_view()),
+    url(r"^detail/", views.detail),
 ]
--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -1,19 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+import pandas as pd
+
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import UserReport, ConsumptionData
 
 # Create your views here.
 
 
-def summary(request):
-    context = {
-        'message': 'Hello!',
-    }
-    return render(request, 'consumption/summary.html', context)
+class SummaryView(ListView):
+    template_name = "consumption/summary.html"
+    model = UserReport
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["subject_name"] = "Consumption"
+        context["total_consumption_label"] = "total consumption"
 
-def detail(request):
-    context = {
-    }
-    return render(request, 'consumption/detail.html', context)
+        data_dir = os.path.join(os.path.dirname(__file__), "..", "..", "data")
+        data_path = os.path.join(data_dir, "consumption", "all.csv")
+        df = pd.read_csv(data_path, index_col="datetime")
+        chart_x = df.index.astype(str).tolist()
+        chart_y = df["consumption"].tolist()
+        context["chart_labels"] = chart_x
+        context["chart_data"] = chart_y
+        return context
+
