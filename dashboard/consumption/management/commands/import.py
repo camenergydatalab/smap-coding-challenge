5baf02b8fc34105f7431d6815b640b0b20016115
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2021-01-26 22:34:02 +0900

    generate summary csv code

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from numpy.lib.function_base import copy
 import pandas as pd
 import glob
 import seaborn as sns
@@ -13,7 +14,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Implement me!")
         consumption_data_paths = glob.glob("../data/consumption/*")
-        print(consumption_data_paths)
         df_list = []
         agg_df = self.read_consumption_data(consumption_data_paths[0])
         for consumption_data_path in consumption_data_paths:
@@ -24,14 +24,56 @@ class Command(BaseCommand):
         # agg_df = self.aggregation(agg_df, df)
         # df = self.read_consumption_data(consumption_data_paths[2])
         # agg_df = self.aggregation(agg_df, df)
+        # agg_df.drop_duplicates()
+        agg_df.to_csv("../data/concat.csv")
         agg_df["average"] = agg_df.mean(axis=1)
         agg_df["total"] = agg_df.sum(axis=1)
-        agg_df.to_csv("../data/concat.csv")
+        sum_df = agg_df[["datetime", "average", "total"]]
+        sum_df.to_csv("../data/summary.csv") 
         
     def aggregation(self,df_list):
-        agg_df = pd.concat(df_list,axis=1).drop_duplicates()
+        base_df = df_list[0]
+        agg_df = pd.concat(df_list,axis=1)
+        agg_df = agg_df.drop_duplicates(subset=None,keep="first")
+        agg_df = agg_df.drop("datetime", axis=1)
         print(agg_df)
+        agg_df = pd.concat([base_df,agg_df], axis=1)
+        agg_df = agg_df.drop_duplicates(subset=None,keep="first")
+        print(agg_df)
+        
         return agg_df
+
+    # def aggregation(self,df_list):
+    #     agg_df = df_list[0]
+    #     # for df in df_list:
+    #     #     print(df)
+    #     #     agg_df = agg_df.merge(df, on="DateTime",copy=False)
+    #     #     agg_df.dropna()
+    #     #     print(agg_df.info())
+    #     #     print(agg_df.describe())
+    #     agg_df = agg_df.merge(df_list[1])
+    #     agg_df = agg_df.merge(df_list[2])
+    #     agg_df = agg_df.merge(df_list[3])
+    #     agg_df = agg_df.merge(df_list[4])
+    #     # agg_df = agg_df.merge(df_list[5])
+    #     # agg_df = agg_df.merge(df_list[6])
+    #     # agg_df = agg_df.merge(df_list[7])
+    #     # agg_df = agg_df.merge(df_list[8])
+    #     # agg_df = agg_df.merge(df_list[9])
+    #     # agg_df = agg_df.merge(df_list[10])
+    #     # agg_df = agg_df.merge(df_list[11])
+    #     # agg_df = agg_df.merge(df_list[12])
+    #     # agg_df = agg_df.merge(df_list[13])
+    #     # agg_df = agg_df.merge(df_list[14])
+    #     # agg_df = agg_df.merge(df_list[15])
+    #     # agg_df = agg_df.merge(df_list[16])
+    #     # agg_df = agg_df.merge(df_list[17])
+    #     # agg_df = agg_df.merge(df_list[18])
+    #     # agg_df = agg_df.merge(df_list[19])
+    #     # agg_df = agg_df.merge(df_list[20])
+    #     # agg_df = agg_df.merge(df_list[21])
+    #     print(agg_df)
+    #     return agg_df
 
     def read_user_data(self):
         user_df = pd.read_csv("../data/user_data.csv")
@@ -41,7 +83,7 @@ class Command(BaseCommand):
         user_id = path.split("/")[-1].split(".")[0]
         df = pd.read_csv(path)
         new_df = df.rename(columns={"consumption":user_id})
-        pd.to_datetime(new_df["datetime"])
+        pd.to_datetime(new_df["datetime"].astype(str),format="%Y-%m-%d %H:%M:%S")
         print(new_df)
         return new_df
 
