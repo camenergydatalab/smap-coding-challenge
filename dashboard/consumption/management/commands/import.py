25a0f76754d8a4e3d7df4fd5599026f51b160b0d
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2021-01-28 01:57:16 +0900

    delete comment out and debug print

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -5,8 +5,6 @@ import glob
 import seaborn as sns
 from pandas.core.frame import DataFrame
 import matplotlib.pyplot as plt
-import base64
-from io import BytesIO
 
 class Command(BaseCommand):
     help = 'import data'
@@ -19,19 +17,11 @@ class Command(BaseCommand):
             df = self.read_consumption_data(consumption_data_path)
             df_list.append(df)
         agg_df = self.aggregation(df_list)
-        # df = self.read_consumption_data(consumption_data_paths[1])
-        # agg_df = self.aggregation(agg_df, df)
-        # df = self.read_consumption_data(consumption_data_paths[2])
-        # agg_df = self.aggregation(agg_df, df)
-        # agg_df.drop_duplicates()
         user_df = pd.read_csv("../data/user_data.csv")
         grouped = user_df.groupby("area").count()
-        print(type(grouped))
-        print(grouped.index.values)
         areas_df_list = []
         for a in grouped.index.values:
             user_list_by_area = user_df[user_df["area"].isin([a])]
-            print(user_list_by_area["id"].tolist())
             area_df_list = []
             for user in user_list_by_area["id"].tolist():
                 df = self.read_consumption_data("../data/consumption/"+ str(user) + ".csv")
@@ -40,12 +30,10 @@ class Command(BaseCommand):
             area_sum_df = pd.DataFrame()
             area_sum_df["datetime"] = area_df["datetime"]
             area_sum_df[a] = area_df.sum(axis=1)
-            print(area_sum_df)
             areas_df_list.append(area_sum_df)
         areas_df = self.aggregation(areas_df_list)
         
         areas_df = areas_df.drop_duplicates()
-        print(areas_df)
         areas_df.to_csv("../data/summary_area.csv")
         sum_df = pd.DataFrame()
         agg_df.to_csv("../data/concat.csv")
@@ -62,38 +50,6 @@ class Command(BaseCommand):
         agg_df = pd.concat([base_df,agg_df], axis=1)
         agg_df = agg_df.drop_duplicates(subset=None,keep="first")
         return agg_df
-
-    # def aggregation(self,df_list):
-    #     agg_df = df_list[0]
-    #     # for df in df_list:
-    #     #     print(df)
-    #     #     agg_df = agg_df.merge(df, on="DateTime",copy=False)
-    #     #     agg_df.dropna()
-    #     #     print(agg_df.info())
-    #     #     print(agg_df.describe())
-    #     agg_df = agg_df.merge(df_list[1])
-    #     agg_df = agg_df.merge(df_list[2])
-    #     agg_df = agg_df.merge(df_list[3])
-    #     agg_df = agg_df.merge(df_list[4])
-    #     # agg_df = agg_df.merge(df_list[5])
-    #     # agg_df = agg_df.merge(df_list[6])
-    #     # agg_df = agg_df.merge(df_list[7])
-    #     # agg_df = agg_df.merge(df_list[8])
-    #     # agg_df = agg_df.merge(df_list[9])
-    #     # agg_df = agg_df.merge(df_list[10])
-    #     # agg_df = agg_df.merge(df_list[11])
-    #     # agg_df = agg_df.merge(df_list[12])
-    #     # agg_df = agg_df.merge(df_list[13])
-    #     # agg_df = agg_df.merge(df_list[14])
-    #     # agg_df = agg_df.merge(df_list[15])
-    #     # agg_df = agg_df.merge(df_list[16])
-    #     # agg_df = agg_df.merge(df_list[17])
-    #     # agg_df = agg_df.merge(df_list[18])
-    #     # agg_df = agg_df.merge(df_list[19])
-    #     # agg_df = agg_df.merge(df_list[20])
-    #     # agg_df = agg_df.merge(df_list[21])
-    #     print(agg_df)
-    #     return agg_df
 
     def read_user_data(self):
         user_df = pd.read_csv("../data/user_data.csv")
--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -7,28 +7,11 @@ import pandas as pd
 import seaborn as sns
 from pandas.core.frame import DataFrame
 import matplotlib.pyplot as plt
-import base64
-from io import BytesIO
-import time
 def summary(request):
-    print(time.time())
     agg_df = pd.read_csv("../data/summary.csv",index_col=0)
-    print(time.time())
-    print(agg_df)
-    x = agg_df["datetime"]
-    y = agg_df["average"]
-    print(time.time())
-    # byte = BytesIO()
-    # sns.lineplot(x=x,y=y)
-    # plt.savefig(byte)
-    # print(time.time())
-    # graph = img_format(byte)
-    # print(time.time())
     summary_by_area = pd.read_csv("../data/summary_area.csv")
-    print(agg_df)
     user_df = pd.read_csv("../data/user_data.csv")
     context = {
-        # "graph":graph,
         "data":agg_df.to_dict(orient='list'),
         "area_data":summary_by_area.to_dict(orient="list"),
         "user_data":user_df.to_dict(orient='records')
@@ -45,8 +28,3 @@ def detail(request,user_id):
         "user_data":d.to_dict(orient="record")[0]
     }
     return render(request, 'consumption/detail.html', context)
-
-def img_format(byte):
-    byte = base64.b64encode(byte.getvalue()).decode("utf-8") 
-    img = "data:image/png;base64,{}".format(byte)
-    return img
