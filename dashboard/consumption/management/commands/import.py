0bce4095d78f8c7d0767f7ea11ae751637e8917e
mamoru0101 <kmamoru0101@gmail.com>
2023-01-01 01:06:41 +0900

    ## Challenge 1 - import コマンドの実装 - modelの実装

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -1,8 +1,72 @@
+import csv
+import glob
+import os
+
 from django.core.management.base import BaseCommand
+
+from consumption.models import UserData, ConsumptionData
 
 
 class Command(BaseCommand):
     help = 'import data'
 
     def handle(self, *args, **options):
-        print("Implement me!")
+        # print("Implement me!")
+
+        # user data
+        user_data_file_path = options["user"]
+        if user_data_file_path:
+            # データ読み込み
+            data = data_from_csv_file(user_data_file_path)
+
+            # データの保存
+            store_user_data(data)
+
+        # consumption data
+        # データ読み込み
+        consumption_data_dir_path = options['consumption']
+        if consumption_data_dir_path:
+            for data_file_path in glob.glob(consumption_data_dir_path + r"\\*.csv"):
+                data = data_from_csv_file(data_file_path)
+
+                # データの保存
+                file_name = os.path.basename(data_file_path)
+                user_id, _ = os.path.splitext(file_name)
+                store_consumption_data(user_id, data)
+
+    def add_arguments(self, parser):
+        parser.add_argument('-u', '--user', type=str)
+        parser.add_argument('-c', '--consumption', type=str)
+
+
+def store_user_data(data: list):
+    """
+    ユーザデータの保存
+    :param data: list
+        [{'id': str, 'area': str, 'tariff': str}, ...]
+    :return: None
+    """
+    UserData.objects.bulk_create([UserData(**item) for item in data])
+
+
+def store_consumption_data(user_id: str, data: list):
+    """
+    :param user_id: str
+        User ID
+    :param data: list
+        [{"datetime": "YYYY-mm-DD HH:MM:SS", "consumption": float}]
+    :return: None
+    """
+    ConsumptionData.objects.bulk_create([ConsumptionData(user_id=user_id, **item) for item in data])
+
+
+def data_from_csv_file(file_path):
+    _, ext = os.path.splitext(file_path)
+
+    if ext != '.csv':
+        raise
+
+    with open(file_path) as f:
+        reader = csv.DictReader(f)
+        data = list(reader)
+    return data
--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class UserData(models.Model):
+    area = models.CharField(max_length=255)
+    tariff = models.CharField(max_length=255)
+
+
+class ConsumptionData(models.Model):
+    user = models.ForeignKey(UserData, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(null=False, blank=False)
+    consumption = models.FloatField(null=False, blank=False)
