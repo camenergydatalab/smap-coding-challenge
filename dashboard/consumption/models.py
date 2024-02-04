b174aecc6a979e07d05110e44a0b04196bc69365
Masaru Fukazawa <upw.ponsuke@gmail.com>
2024-02-04 18:06:44 +0900

    リントチェックを反映

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 class Area(models.Model):
     """エリア情報"""
@@ -34,6 +35,7 @@ class User(models.Model):
 
     def __str__(self):
         return str(self.id)
+
 
 class Consumption(models.Model):
     """消費データ"""
