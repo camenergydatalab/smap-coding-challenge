b584a6e4660795be5cd87e5b4d974e8816e0ca19
Masaru Fukazawa <upw.ponsuke@gmail.com>
2024-02-12 08:25:52 +0900

    実装とコメントに差異があったため修正

--- dashboard/static/js/line_chart.js
+++ dashboard/static/js/line_chart.js
@@ -2,7 +2,7 @@
  * 名前 : display_line_chart
  * 処理 : chart.jsを用いて、折れ線グラフを生成、描画する
  * 引数 : 
- *      element_id : 折れ線グラフを描画するcanvasタグの要素オブジェクト
+ *      ctx : 折れ線グラフを描画するcanvasタグの要素オブジェクト
  *      chart_data_json : Chartクラスのdataに渡すJSONデータ
  */
 
