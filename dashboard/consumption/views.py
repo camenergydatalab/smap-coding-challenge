cfb4e8976820e7334e90ccb16f978f54a7aeb008
Masaru Fukazawa <upw.ponsuke@gmail.com>
2024-02-11 21:11:43 +0900

    集計期間を取得し表示する

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -12,50 +12,56 @@ from .repository.user_repository import UserRepository
 
 # Create your views here.
 def summary(request):
-    # 集計期間の取得
-    labels = ConsumptionRepository.get_total_period_months()
 
     users = UserRepository.get_all()
 
-    user_table_datas = []
+    # 集計期間の取得
+    total_period_months = ConsumptionRepository.get_total_period_months()
+
+    # 集計期間の開始日と終了日を取得
+    start_end_day = ConsumptionRepository.get_period_start_end_day()
+
     total_value_chart_datasets = []
     daily_average_value_chart_datasets = []
 
-    # 月別消費量合計
-    total_value_per_month = ConsumptionRepository.get_total_value_per_month()
+    if total_period_months.count:
 
-    total_value_chart_datasets.append(
-        {
-            "label": "月別 消費量合計",
-            "data": list(map(str, list(total_value_per_month.values()))),
-            "borderWidth": 2,
-        }
-    )
+        # 月別消費量合計
+        total_value_per_month = ConsumptionRepository.get_total_value_per_month()
 
-    # 月別1日あたりの平均消費量
-    daily_average_value_per_month = (
-        ConsumptionRepository.get_daily_average_value_per_month()
-    )
+        total_value_chart_datasets.append(
+            {
+                "label": "月別 消費量合計",
+                "data": list(map(str, list(total_value_per_month.values()))),
+                "borderWidth": 2,
+            }
+        )
 
-    daily_average_value_chart_datasets.append(
-        {
-            "label": "月別 1日あたりの平均消費量",
-            "data": list(map(str, list(daily_average_value_per_month.values()))),
-            "borderWidth": 2,
-        }
-    )
+        # 月別1日あたりの平均消費量
+        daily_average_value_per_month = (
+            ConsumptionRepository.get_daily_average_value_per_month()
+        )
+
+        daily_average_value_chart_datasets.append(
+            {
+                "label": "月別 1日あたりの平均消費量",
+                "data": list(map(str, list(daily_average_value_per_month.values()))),
+                "borderWidth": 2,
+            }
+        )
 
     total_value_chart_data = {
-        "labels": labels,
+        "labels": total_period_months,
         "datasets": total_value_chart_datasets,
     }
 
     daily_average_value_chart_data = {
-        "labels": labels,
+        "labels": total_period_months,
         "datasets": daily_average_value_chart_datasets,
     }
 
     context = {
+        "start_end_day": start_end_day,
         "users": users,
         "total_value_chart_data": json.dumps(total_value_chart_data),
         "daily_average_value_chart_data": json.dumps(daily_average_value_chart_data),
@@ -65,50 +71,58 @@ def summary(request):
 
 
 def detail(request, user_id):
+
     user = get_object_or_404(User, pk=user_id)
 
-    labels = ConsumptionRepository.get_total_period_months()
+    # 集計期間の取得
+    total_period_months = ConsumptionRepository.get_total_period_months()
+
+    # 集計期間の開始日と終了日を取得
+    start_end_day = ConsumptionRepository.get_period_start_end_day()
 
     total_value_chart_datasets = []
     daily_average_value_chart_datasets = []
 
-    # ユーザごとの月別消費量合計
-    total_value_per_month = ConsumptionRepository.get_total_value_per_month_by_user_id(
-        user
-    )
+    if total_period_months.count:
 
-    total_value_chart_datasets.append(
-        {
-            "label": "ユーザID : " + str(user),
-            "data": list(map(str, list(total_value_per_month.values()))),
-            "borderWidth": 2,
-        }
-    )
+        # ユーザごとの月別消費量合計
+        total_value_per_month = ConsumptionRepository.get_total_value_per_month_by_user_id(
+            user
+        )
 
-    # ユーザごとの1日あたりの平均消費量
-    daily_average_value_per_month = (
-        ConsumptionRepository.get_daily_average_value_per_month_by_user_id(user)
-    )
+        total_value_chart_datasets.append(
+            {
+                "label": "ユーザID : " + str(user),
+                "data": list(map(str, list(total_value_per_month.values()))),
+                "borderWidth": 2,
+            }
+        )
 
-    daily_average_value_chart_datasets.append(
-        {
-            "label": "ユーザID : " + str(user),
-            "data": list(map(str, list(daily_average_value_per_month.values()))),
-            "borderWidth": 2,
-        }
-    )
+        # ユーザごとの1日あたりの平均消費量
+        daily_average_value_per_month = (
+            ConsumptionRepository.get_daily_average_value_per_month_by_user_id(user)
+        )
+
+        daily_average_value_chart_datasets.append(
+            {
+                "label": "ユーザID : " + str(user),
+                "data": list(map(str, list(daily_average_value_per_month.values()))),
+                "borderWidth": 2,
+            }
+        )
 
     total_value_chart_data = {
-        "labels": labels,
+        "labels": total_period_months,
         "datasets": total_value_chart_datasets,
     }
 
     daily_average_value_chart_data = {
-        "labels": labels,
+        "labels": total_period_months,
         "datasets": daily_average_value_chart_datasets,
     }
 
     context = {
+        "start_end_day": start_end_day,
         "user": user,
         "total_value_chart_data": json.dumps(total_value_chart_data),
         "daily_average_value_chart_data": json.dumps(daily_average_value_chart_data),
