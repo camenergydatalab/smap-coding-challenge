ab4ec33978710df9d24465741bb203120716faec
yuyaosako <yuyaosako070864@gmail.com>
2024-03-06 22:39:43 +0900

    画面周りの実装

--- dashboard/consumption/urls.py
+++ dashboard/consumption/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
+app_name = "consumption"
 urlpatterns = [
     url(r"^$", views.summary),
     url(r"^summary/", views.summary),
-    url(r"^detail/", views.detail),
+    path("detail/<str:user_id>/", views.detail, name="detail"),
 ]
